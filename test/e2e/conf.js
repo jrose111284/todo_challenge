27e610b6dabfbde73dc8c09649a1e33631f42d5c
jamierose <jrose111284@gmail.com>
2015-12-05 23:38:56 +0000

    added 3 feature test plus css

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -2,8 +2,6 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['todo-spec.js'],
   multiCapabilities: [{
-    browserName: 'firefox'
-  }, {
     browserName: 'chrome'
   }]
 }
--- test/e2e/todo-spec.js
+++ test/e2e/todo-spec.js
@@ -1,13 +1,35 @@
 describe('Todo list', function() {
+  beforeEach(function(){
+      browser.get('http://localhost:8080');
+  })
 
+  it('has a title', function(){
+    expect(browser.getTitle()).toEqual('Todo');
+  });
+});
+
+describe('will add a todo list', function() {
   it('should add a todo', function() {
     browser.get('http://localhost:8080');
 
-    element(by.model('toDoList.newTodo')).sendKeys('write first protractor test');
-    element(by.css('[id=totalcount]')).click();
+    element(by.model('toDoList.newTodo')).sendKeys('get milk, bread and cheese');
+    element(by.css('[id="submit"]')).click();
 
     var todoList = element.all(by.repeater('todo in toDoList.list'));
-    expect(toDoList.addToList()).toEqual(0);
+    expect(todoList.count()).toEqual(1);
+    expect(todoList.get(0).getText()).toEqual('get milk, bread and cheese');
+  });
+});
+
+describe('will remove a todo list', function() {
+  it('should remove a todo', function() {
+    browser.get('http://localhost:8080');
+
+    element(by.model('toDoList.newTodo')).sendKeys('get milk, bread and cheese');
+    element(by.css('[id="submit"]')).click();
+    element(by.css('[id="clear"]')).click();
+
+    expect(todoList.count()).toEqual(0);
 
   });
 });
