bccf7675a059eb5e27752c89e7cef433ae4a2c56
jamierose <jrose111284@gmail.com>
2015-12-05 12:59:27 +0000

    fixed clear button

--- js/todoController.js
+++ js/todoController.js
@@ -4,18 +4,18 @@ todoApp.controller('todoController', [function () {
 
   self.list = []
 
+  this.time = new Date();
 
   self.addToList = function () {
-    self.list.push({'title': self.newTodo,
-                    'done':false});
+    self.list.push({ title: self.newTodo,
+                    done :false});
     self.newTodo = ''
   };
-
 
   self.clear = function(){
     self.list = self.list.filter(function(item){
       return !item.done
     })
-  }
+  };
 
 }]);
--- test/todoController.spec.js
+++ test/todoController.spec.js
@@ -1,1 +1,10 @@
 
+describe('GitUserSearchController', function() {
+  beforeEach(module('todoApp'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('todoController');
+  }));
+});
